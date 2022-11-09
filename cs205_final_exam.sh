3b5356bd0dee0f6de6cb4f2deec60ea805c81b91
Math1029 <haastiin@protonmail.com>
2022-11-09 16:02:20 -0700

    Did line count and file name

--- cs205_final_exam.sh
+++ cs205_final_exam.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # TODO: Modify this file to create a shell script that is able to use awk to go through a file formatted like pokemon.dat and provides a printed report in the following format (where your script correctly calculates the values that go into the [VALUE] placeholders):
 # ===== SUMMARY OF DATA FILE =====
 #    File name: [VALUE]
@@ -10,3 +12,15 @@
 # The spacing and header formatting should match the above formatting description exactly.
 # There should be a comment explaining the purpose of each line in your shell script. 
 # The data file will be passed in to the script as a positional parameter and will not necessarily be called pokemon.dat. However, you can assume that any file passed to this script will be formatted exactly the way pokemon.dat is formatted.
+
+NAME=$1;
+TOTAL=`cat $1 | wc -l`;
+
+echo "
+=====SUMARY OF DATA FILE =====
+	File name: $NAME
+	Total Pokemon: $TOTAL
+	Avg. HP: $HP
+	Avg. Attack:
+===== END SUMMARY =====
+"
