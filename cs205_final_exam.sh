1676b4715bbdc866a54f7e5518efe91eeb62462f
Math1029 <haastiin@protonmail.com>
2022-11-10 09:27:08 -0700

    I have finally completed the variable!! Wohoooo!

--- cs205_final_exam.sh
+++ cs205_final_exam.sh
@@ -14,13 +14,37 @@
 # The data file will be passed in to the script as a positional parameter and will not necessarily be called pokemon.dat. However, you can assume that any file passed to this script will be formatted exactly the way pokemon.dat is formatted.
 
 NAME=$1;
-TOTAL=`cat $1 | wc -l`;
+TOTAL=` awk 'END{print NR -1};' $1`;
+HP=`awk '
+BEGIN{FS="\t"}
+BEGIN{
+	sum = 0
+	lines = 0
+}
+{
+	sum += $6
+	lines += 1
+}
+END {if (lines > 0) print sum / (lines-1)}
+' $1`;
+ATK=`awk '
+BEGIN{FS="\t"}
+BEGIN{
+	sum = 0
+	lines = 0
+}
+{
+	sum += $7
+	lines += 1
+}
+END {if (lines > 0) print sum / (lines-1)}
 
+' $1`;
 echo "
 =====SUMARY OF DATA FILE =====
 	File name: $NAME
 	Total Pokemon: $TOTAL
 	Avg. HP: $HP
-	Avg. Attack:
+	Avg. Attack: $ATK
 ===== END SUMMARY =====
 "
